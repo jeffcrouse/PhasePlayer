042e71ec2f5dab711ac28d55424bf06300dcc6ee
Jeff Crouse <jefftimesten@gmail.com>
2015-11-19 16:39:55 -0500

    read video file from downloads

--- app/src/main/java/org/gearvrf/simplesample/SampleViewManager.java
+++ app/src/main/java/org/gearvrf/simplesample/SampleViewManager.java
@@ -25,7 +25,7 @@ import org.gearvrf.GVRAndroidResource;
 import org.gearvrf.GVRCameraRig;
 import org.gearvrf.GVRContext;
 import org.gearvrf.GVRMesh;
-import org.gearvrf.GVRPostEffect;
+//import org.gearvrf.GVRPostEffect;
 //import org.gearvrf.GVRRenderData.GVRRenderMaskBit;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
@@ -38,8 +38,13 @@ import org.gearvrf.animation.GVRAnimationEngine;
 //import org.gearvrf.animation.GVRScaleAnimation;
 import org.gearvrf.scene_objects.GVRVideoSceneObject;
 import org.gearvrf.scene_objects.GVRVideoSceneObject.GVRVideoType;
-
+//import android.provider.Settings.Secure;
+import 	java.io.File;
+import android.util.Log;
+import android.os.Environment;
 import android.media.MediaPlayer;
+import android.net.Uri;
+
 
 public class SampleViewManager extends GVRScript {
 
@@ -67,7 +72,19 @@ public class SampleViewManager extends GVRScript {
         GVRMesh sphereMesh = mGVRContext.loadMesh(new GVRAndroidResource(
                 mGVRContext, R.raw.sphere_mesh_5));
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(mGVRContext.getContext(), R.drawable.seven);
+        String downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        Log.d("Files", "Path: " + downloadsPath);
+        File f = new File(downloadsPath);
+        File file[] = f.listFiles();
+        Log.d("Files", "Size: " + file.length);
+        for (int i=0; i < file.length; i++)
+        {
+            Log.d("Files", "FileName:" + file[i].getName());
+        }
+
+        // new File(downloadsPath+"/360.mp4")
+        Uri videoURI = Uri.fromFile(file[file.length-1]);
+        MediaPlayer mediaPlayer = MediaPlayer.create(mGVRContext.getContext(), videoURI); //R.drawable.video);
         mediaPlayer.setLooping(true);
         mediaPlayer.start();
 
